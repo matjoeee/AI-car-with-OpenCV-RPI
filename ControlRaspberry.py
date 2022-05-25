9cb8afd7668483e35c91ecdf8bb21e7f419c2b38
matjoeee <88538788+matjoeee@users.noreply.github.com>
2022-05-26 01:07:53 +0200

    Update ControlRaspberry.py

--- ControlRaspberry.py
+++ ControlRaspberry.py
@@ -1,96 +1,105 @@
-# a code by "https://github.com/mohammadreza-sharifi"
+# a code based on the code of "https://github.com/mohammadreza-sharifi"
 # used in the Practice Enterprice project of Raul Lopez & Matthew Wuyts
 # students at Thomas More, Belgium
 
-# the libraries
-import RPi.GPIO as io
+# import the desired libraries
+import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
+import time
 
-# extra configurations
-io.setwarnings(False)
-io.setmode(io.BOARD)
-
-
-# setup driver pins
-io.setup(3, io.OUT)  # IN1
-io.setup(5, io.OUT)  # IN2
-io.setup(8, io.OUT)  # IN3
-io.setup(10, io.OUT)  # IN4
-
-# the movement functions
-# this function moves the robot forward
-def func_forward():
-    io.output(3, 1)
-    io.output(5, 0)
-    io.output(8, 1)
-    io.output(10, 0)
-
-# this function moves the robot backward
-def func_backward():
-    io.output(3, 0)
-    io.output(5, 1)
-    io.output(8, 0)
-    io.output(10, 1)
-
-# this function stops the robot
-def func_stop():
-    io.output(3, 0)
-    io.output(5, 0)
-    io.output(8, 0)
-    io.output(10, 0)
-
-# this function moves the robot right
-def func_right():
-    io.output(3, 1)
-    io.output(5, 0)
-    io.output(8, 0)
-    io.output(10, 0)
-
-# this function moves the robot left
-def func_left():
-    io.output(3, 0)
-    io.output(5, 0)
-    io.output(8, 1)
-    io.output(10, 0)
+# defining the pins
+# GPIO5 = 2
+# GPIO6 = 3
+# GPIO12 = 14
+# GPIO13 = 15
 
 
+# disable warnings
+GPIO.setwarnings(False)
 
-MQTT_SERVER = "localhost"  # specify the broker address, it can be IP of raspberry pi or simply localhost
+
+# set to correct mode
+GPIO.setmode(GPIO.BCM)
+
+
+# pin setup
+GPIO.setup(2, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(14, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+
+
+# define the movement functions
+def move_forward():
+    print("forward")
+    GPIO.output(GPIO5, 1)
+    GPIO.output(3, 0)
+    GPIO.output(14, 1)
+    GPIO.output(15, 0)
+
+def move_backward():
+    GPIO.output(2, 0)
+    GPIO.output(3, 1)
+    GPIO.output(14, 0)
+    GPIO.output(15, 1)
+
+def move_right():
+    GPIO.output(2, 1)
+    GPIO.output(3, 0)
+    GPIO.output(14, 0)
+    GPIO.output(15, 0)
+
+def move_left():
+    GPIO.output(2, 0)
+    GPIO.output(3, 0)
+    GPIO.output(14, 1)
+    GPIO.output(15, 0)
+
+def complete_stop():
+    GPIO.output(2, 0)
+    GPIO.output(3, 0)
+    GPIO.output(14, 0)
+    GPIO.output(15, 0)
+
+
+# set up the server link
+MQTT_SERVER = "127.0.0.1"
 MQTT_PATH = "test_channel"
 
-
-# defining extra functions
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
+    print("You have connected successfully with result code " + str(rc))
     client.subscribe(MQTT_PATH)
+    print("success connecting to " + MQTT_PATH)
 
 def on_message(client, userdata, message):
-    # print("Received message '" + str(message.payload) + "' on topic '" + message.topic)
-    if message.payload == b'5':
-        func_forward()
-    elif message.payload == b'0':
-        func_stop()
-    elif message.payload == b'4':
-        func_backward()
-    elif message.payload == b'1':
-        func_left()
-    elif message.payload == b'2':
-        func_right()
-    else:
-        func_stop()
+    print(message.payload)
 
+    # tell program what to do when certain amount of fingers received
+    if message.payload == b'8':
+        move_forward()
+    elif message.payload == b'4':
+        move_backward()
+    elif message.payload == b'2':
+        move_right()
+    elif message.payload == b'1':
+        move_left()
+    elif message.payload == b'0':
+        complete_stop()
+    else:
+        complete_stop()
+
+
+# define the main program
 def main():
-    mqtt_client = mqtt.Client()
+    mqtt_client = mqtt.Client("client")
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
 
     mqtt_client.connect(MQTT_SERVER, 1883, 60)
-    # Connect to the MQTT server and process messages in a background thread.
     mqtt_client.loop_start()
-
+    while(1):
+        time.sleep(1)
 
 if __name__ == '__main__':
-    print('MQTT to InfluxDB bridge')
+    print('Bridging MQTT to InfluxDB.')
     main()
